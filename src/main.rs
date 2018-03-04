baa60789a163090737cf5206d0c51f8e93793eac
Jesse Luehrs <doy@tozt.net>
2018-03-04 17:22:29 -0500

    add version and author data to command line options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate chrono;
+#[macro_use]
 extern crate clap;
 extern crate git2;
 extern crate hostname;
@@ -22,7 +23,9 @@ fn collect_data() -> prompt::PromptData {
 
     let matches = clap::App::new("fancy-prompt")
         .about("Prints a fancy prompt")
-        // XXX author, version (extract from cargo)
+        .author(crate_authors!())
+        .version(crate_version!())
+        .long_version(option_env!("FANCY_PROMPT_BUILD_GIT_REV").unwrap())
         .arg(clap::Arg::with_name("prompt-escape")
              .long("prompt-escape")
              .value_name("SHELL")
